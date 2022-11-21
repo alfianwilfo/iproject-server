43581887a35b2ba1d1fcb1e5092dcb3eed023ba4
alfian <alfianwilfredohoris@gmail.com>
2022-11-21 10:34:51 +0700

    login with username or email on progress

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,28 @@
 let { User } = require("../models/index");
 let { comparePassword } = require("../helpers/bcrypt");
 let { createToken } = require("../helpers/jwt");
+const { use } = require("../routes");
+const { query } = require("express");
 class UserController {
   static async login(req, res, next) {
     try {
-      let { email, password } = req.body;
-      let findUser = await User.findOne({ where: { email: email } });
+      let { email, username, password } = req.body;
+      let query;
+      if (email) {
+        query = { where: { email: email } };
+      } else if (username) {
+        query = { where: { username: username } };
+      } else if (!email || !username) {
+        throw { name: "BAD_REQUEST" };
+      }
+
+      let findUser = await User.findOne(query);
+      console.log(findUser, "?????");
       if (!findUser) {
         throw { name: "USER_NOT_FOUND" };
       }
       let pw = await comparePassword(password, findUser.password);
+      console.log(pw, "====");
       if (!pw) {
         throw { name: "PASSWORD_WRONG" };
       }
@@ -17,7 +30,7 @@ class UserController {
       let token = createToken(payload);
       console.log(token);
     } catch (error) {
-      console.log(error);
+      console.log(error, "<<<");
       next(error);
     }
   }
