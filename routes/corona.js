9b19a516a9bb0e89c4d5bedfa5f8052802db0389
alfian <alfianwilfredohoris@gmail.com>
2022-12-06 17:21:46 +0700

    endpoint get statistic and get history

--- routes/corona.js
+++ routes/corona.js
@@ -3,5 +3,7 @@ const app = express();
 let coronaController = require("../controllers/coronaController");
 
 app.get("/countries", coronaController.getCountry);
+app.get("/statistics", coronaController.getStatistic);
+app.get("/history", coronaController.getHistory);
 
 module.exports = app;
